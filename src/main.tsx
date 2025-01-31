d90a5df535b103077f29e61ed7d72e576951361a
Rasel <raselmiahdev@gmail.com>
2025-01-31 15:51:02 +0600

    Loading service complete

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { LoadingProvider } from "./contexts/loading-context.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <LoadingProvider>
+        <App />
+      </LoadingProvider>
+    </BrowserRouter>
   </StrictMode>
 );
--- src/view/router/root.router.tsx
+++ src/view/router/root.router.tsx
@@ -1,9 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import NotFoundPage from "../page/not-found/not-found.page"
-// import LoginPage from "../page/login/login.page"
-// import ProtectedRoute from "./protected.route"
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "../page/home/home.page";
-// import PublicRoute from "./public.route"
 import { RouteUrl } from "./url";
 import SearchResultPage from "../page/search-result/search-result.page";
 import SingleProfilePage from "../page/single-profile/single-profile.page";
@@ -12,35 +8,32 @@ import ReportPage from "../page/report/report.page";
 import LoginPage from "../page/login/login.page";
 import SignUpPage from "../page/sign-up/sign-up.page";
 import AboutYouPage from "../page/about-you/about-you.page";
+import { useLoading } from "../../contexts/loading-context";
+import { useEffect } from "react";
+import LoadingSpinner from "../components/common/loading-spinner";
 
 export default function RootRouter() {
+  const location = useLocation();
+  const { loading, setLoading } = useLoading();
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 500);
+    return () => clearTimeout(timer);
+  }, [location.pathname, setLoading]);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path={RouteUrl.HOME} element={<HomePage />} />
-          <Route path={RouteUrl.SEARCH_RESULT} element={<SearchResultPage />} />
-          <Route path={RouteUrl.RATING} element={<RatingPage />} />
-          <Route path={RouteUrl.REPORT} element={<ReportPage />} />
-          <Route path={RouteUrl.LOGIN} element={<LoginPage />} />
-          <Route path={RouteUrl.SIGN_UP} element={<SignUpPage />} />
-          <Route path={RouteUrl.ABOUT_YOU} element={<AboutYouPage />} />
-          <Route
-            path={RouteUrl.SINGLE_PROFILE}
-            element={<SingleProfilePage />}
-          />
-          {/* <Route
-                        path={RouteUrl.LOGIN}
-                        element={
-                            <PublicRoute>
-                                <LoginPage />
-                            </PublicRoute>
-                        }
-                    /> */}
-
-          {/* <Route path={RouteUrl.NOT_FOUND} element={<NotFoundPage />} /> */}
-        </Routes>
-      </BrowserRouter>
+      {loading && <LoadingSpinner />}
+      <Routes>
+        <Route path={RouteUrl.HOME} element={<HomePage />} />
+        <Route path={RouteUrl.SEARCH_RESULT} element={<SearchResultPage />} />
+        <Route path={RouteUrl.RATING} element={<RatingPage />} />
+        <Route path={RouteUrl.REPORT} element={<ReportPage />} />
+        <Route path={RouteUrl.LOGIN} element={<LoginPage />} />
+        <Route path={RouteUrl.SIGN_UP} element={<SignUpPage />} />
+        <Route path={RouteUrl.ABOUT_YOU} element={<AboutYouPage />} />
+        <Route path={RouteUrl.SINGLE_PROFILE} element={<SingleProfilePage />} />
+      </Routes>
     </>
   );
 }
